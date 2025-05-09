b3cd8575564e249a4313e2c47d9cd73c0de1d94c
Matvey Sudomoykin <me@matveymatvey.com>
2025-05-09 07:53:08 +0300

    buttons and screen interactive

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import './scss/styles.scss';
+
+const screen = document.querySelector('.phone__screen');
+const buttons = document.querySelectorAll('.phone__button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(`Button clicked! ${button.textContent}`);
+        
+        if (screen) {
+            screen.textContent += `${button.textContent}`;
+        }
+    });
+});
