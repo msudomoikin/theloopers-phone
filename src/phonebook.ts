ffbc946149aebd5de62f625c398bf78720911959
Matvey Sudomoykin <me@matveymatvey.com>
2025-05-18 01:09:45 +0300

    change nymbers to arrays with one element

--- src/phonebook.ts
+++ src/phonebook.ts
@@ -1,7 +1,7 @@
 type Phonebook = {
     [country: string]: {
-        dialTone: { hz: number[] | number; on: number; off: number };
-        busyTone: { hz: number; on: number; off: number };
+        dialTone: { hz: number[]; on: number; off: number };
+        busyTone: { hz: number[]; on: number; off: number };
         pattern?: RegExp; 
     };
 };
@@ -9,16 +9,16 @@ type Phonebook = {
 export const phonebook: Phonebook = {
     Albania: {
         dialTone: { hz: [425, 24], on: 1, off: 4 },
-        busyTone: { hz: 425, on: 0.5, off: 0.5 },
+        busyTone: { hz: [425], on: 0.5, off: 0.5 },
         pattern: /^355/,
     },
     Andorra: {
-        dialTone: { hz: 425, on: 1.5, off: 3 },
-        busyTone: { hz: 425, on: 0.75, off: 0.75 },
+        dialTone: { hz: [425], on: 1.5, off: 3 },
+        busyTone: { hz: [425], on: 0.75, off: 0.75 },
         pattern: /^376/,
     },
     SAR: {
-        dialTone: { hz: 400, on: 1, off: 1.5 },
-        busyTone: { hz: 400, on: 0.5, off: 0.5 },
+        dialTone: { hz: [400], on: 1, off: 1.5 },
+        busyTone: { hz: [400], on: 0.5, off: 0.5 },
     },
 };
