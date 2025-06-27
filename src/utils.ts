ec75bc37baeca282541f30a4e5854bbe7a25c266
Matvey Sudomoykin <me@matveymatvey.com>
2025-06-27 18:49:14 +0300

    return dial tone from country code

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,17 @@
-import { phonebook } from './phonebook';
+import { phonebookRecords } from "./Phonebook"
 
-export function findMatchingCountry(number: string): string | undefined {
-    return Object.keys(phonebook).find((country) => {
-        const pattern = phonebook[country].code;
-        return pattern?.test(number);
-    });
+
+export function findRingingTone(number: string): string | undefined {
+    const record = phonebookRecords.find((record) => {
+        return record.code?.test(number)
+    })
+
+    return record?.ringTone
+}
+
+export function isTouchDevice(): boolean {
+    return (
+        "ontouchstart" in window ||
+        navigator.maxTouchPoints > 0
+    );
 }
